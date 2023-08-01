9717c188274c001a2de61d187ac8ff4deea87a1c
nice <gitea@henull.com>
2023-08-01 08:03:48 +0700

    Refactor error handling in main.go and engine.go
    - Refactored the code in engine.go to handle path parsing failure more accurately.
    - Updated error handling in main.go to log errors and return appropriate HTTP status codes.

--- main.go
+++ main.go
@@ -12,22 +12,32 @@ func proxy(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	engine := api.NewApiEngine()
 	if err := engine.HandleAuthorization(r.Header.Get("authorization")); err != nil {
+		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusForbidden)
+		return
 	}
 	if err := engine.HandlePath(r.URL.Path); err != nil {
+		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusBadRequest)
+		return
 	}
 	handle := api.NewApiHandle(engine, r.Host)
 	if err := handle.New(r.Method, r.Body); err != nil {
+		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusBadRequest)
+		return
 	}
 	handle.RequestHeaders(r.Header)
 	if err := handle.Do(); err != nil {
+		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return
 	}
 	handle.ResponseHeaders(w)
 	if err := handle.Write(w); err != nil {
+		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		return
 	}
 }
 func main() {
