278dc8073b3d9a5d149bf395086eb1feee5defa4
nice <gitea@henull.com>
2023-08-01 08:07:17 +0700

    Refactor error handling in proxy function and update version number to 0.1.5

--- main.go
+++ main.go
@@ -12,36 +12,31 @@ func proxy(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	engine := api.NewApiEngine()
 	if err := engine.HandleAuthorization(r.Header.Get("authorization")); err != nil {
-		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusForbidden)
 		return
 	}
 	if err := engine.HandlePath(r.URL.Path); err != nil {
-		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusBadRequest)
 		return
 	}
 	handle := api.NewApiHandle(engine, r.Host)
 	if err := handle.New(r.Method, r.Body); err != nil {
-		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusBadRequest)
 		return
 	}
 	handle.RequestHeaders(r.Header)
 	if err := handle.Do(); err != nil {
-		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
 		return
 	}
 	handle.ResponseHeaders(w)
 	if err := handle.Write(w); err != nil {
-		log.Println(err)
 		http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
 		return
 	}
 }
 func main() {
-	log.Println("[0.1.3] ListenAndServe :8080")
+	log.Println("[0.1.5] ListenAndServe :8080")
 	if err := http.ListenAndServe(":8080", http.HandlerFunc(proxy)); err != nil {
 		log.Panicln(err)
 	}
